9e6b4795f8b2757891746cabe056911943fc91b7
JDempe <123279032+JDempe@users.noreply.github.com>
2023-02-12 22:43:40 -0500

    Formatted profile area

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Credit: https://stackoverflow.com/questions/6398526/javascript-jquery-or-something-to-change-text-every-some-seconds
+var text = ["Mechanical Engineer", "Test Engineer", "Full Stack Developer"];
+var counter = 0;
+var elem = document.getElementById("changeText");
+elem.innerHTML = text[0];
+var opacity = Number(window.getComputedStyle(elem).getPropertyValue("opacity"));
+var inst = setInterval(change, 4000);
+
+function change() {
+   counter++;
+   if (counter >= text.length) {
+      counter = 0;
+   };
+   elem.innerHTML = text[counter];
+}
